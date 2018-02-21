368011bc486a29087b2657a0070451253dfe44b9
Pamela Rojas <32311210+Azur-Azabache@users.noreply.github.com>
2018-02-21 10:42:48 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function(){
   var $mostrarTemas = $('#mostrarTemas');
   var todosLosTemas = {
-    url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics'
+    url: 'https://examen-laboratoria-sprint-5.herokuapp.com/topics'
   };
 
   var cargarPagina = function() {
@@ -15,37 +15,49 @@ $(document).ready(function() {
   };
 
   var crearTema = function(tema) {
+
     var $contenedorTema = $('<div />');
     $contenedorTema.addClass('jumbotron');
-    $contenedorTema.append('<h6>Número de tema: ' + tema.id + '</h6>');
-    $contenedorTema.append('<h6><strong>Autor: </strong>' + tema.author_name + '</h6>');
-    $contenedorTema.append('<h6><strong>Contenido: </strong>' + tema.content + '</h6>');
-    $contenedorTema.append('<h6>Número de respuestas: ' + tema.responses_count + '</h6>');
+    $contenedorTema.append("<h6>Número de tema: "+ tema.id +"</h6>");
+    $contenedorTema.append("<h6><strong>Autor: </strong>"+tema.author_name +"</h6>");
+    $contenedorTema.append("<h6><strong>Contenido: </strong><span class='willSearch'>"+ tema.content +"</span></h6>");
+    $contenedorTema.append("<h6>Número de respuestas: "+ tema.responses_count +"</h6>");
     $mostrarTemas.append($contenedorTema);
+
   };
 
-  $('#addTopic').on('click', function() {
+  $('#addTopic').on('click', function(){
+
     $.post(todosLosTemas.url, {
       'author_name': $('#autor-tema').val(),
       'content': $('#contenido').val()
     }, function(tema) {
+
       var $contenedorTema = $('<div />');
       $contenedorTema.addClass('jumbotron');
-      $contenedorTema.append('<h6>Número de tema: ' + tema.id + '</h6>');
-      $contenedorTema.append('<h6><strong>Autor: </strong>' + tema.author_name + '</h6>');
-      $contenedorTema.append('<h6><strong>Contenido: </strong>' + tema.content + '</h6>');
-      $contenedorTema.append('<h6>Número de respuestas: 0</h6>');
+      $contenedorTema.append("<h6>Número de tema: "+ tema.id +"</h6>");
+      $contenedorTema.append("<h6><strong>Autor: </strong>"+tema.author_name +"</h6>");
+      $contenedorTema.append("<h6><strong>Contenido: </strong><span class='willSearch'>"+ tema.content +"</span></h6>");
+      $contenedorTema.append("<h6>Número de respuestas: 0</h6>");
       $mostrarTemas.prepend($contenedorTema);
+
     });
     console.log('se esconde el modal');
     $('#myModal').modal('hide');
-  });
+  })
 
-  $('#btnSearch').on('click', function() {
-    $('.jumbotron').hide();
+  $('#btnSearch').on('click', function(){
     var word = $('#search').val().toLowerCase();
-    $('.jumbotron:contains(' + word + ')').show();
+    if(word !== ''){
+      $( ".jumbotron" ).hide();
+      var element = $('.willSearch:contains('+ word +')');
+      console.log(element);
+      $( element ).closest( "div.jumbotron" ).show();
+    } else {
+      alert('Ingresa una palabra válida');
+    }
   });
 
   cargarPagina();
+
 });
