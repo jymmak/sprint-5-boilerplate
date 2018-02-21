53b1a1ea0647a399971b9ad54f0258040a8bc30c
Azur_Azabache <tharuka_20@hotmail.com>
2018-02-21 02:58:43 -0500

    update

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
 
   var cargarPagina = function() {
     mostrarTodosTemas();
-
   };
 
   var mostrarTodosTemas = function() {
@@ -28,12 +27,12 @@ $(document).ready(function(){
   };
 
   $('#addTopic').on('click', function(){
-    console.log('incicia el evento');
+
     $.post(todosLosTemas.url, {
       'author_name': $('#autor-tema').val(),
       'content': $('#contenido').val()
     }, function(tema) {
-      console.log('entro a la funcion');
+
       var $contenedorTema = $('<div />');
       $contenedorTema.addClass('jumbotron');
       $contenedorTema.append("<h6>Número de tema: "+ tema.id +"</h6>");
@@ -48,10 +47,10 @@ $(document).ready(function(){
   })
 
   $('#btnSearch').on('click', function(){
-  $('#mostrarTemas').empty();
-  
+    $( ".jumbotron" ).hide();
+  var word = $('#search').val().toLowerCase();
+    $( ".jumbotron:contains("+ word +")").show();
   });
-
 
   cargarPagina();
 
